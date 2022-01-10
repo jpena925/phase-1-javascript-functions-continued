4437bc005a35af8d5d521a419d04058b8139c257
Jack Pena <penajackc@gmail.com>
2022-01-10 15:53:30 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(verb = "roller-skate"){
+    return `This Saturday, I want to ${verb}!`;
+};
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = "*"){
+    return function(word = "special"){
+        return `You are ${flair}${word}${flair}!`;
+    };
+};
